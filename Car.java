25022ee1924cead7f04cf6faff311489ebde3cde
csramya123 <139791710+csramya123@users.noreply.github.com>
2024-03-01 17:41:54 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+	
+		//datatype variable=value
+		int price=100000;		
+		String brandName="Tata"; //camalCase		
+		//String hold collection of characters
+		char logo='T';		
+		float fuel=12.45f;	
+		//String fuelCapacity="12.45lt";
+		double modelVariant=456.2343565757;
+		boolean isNew=false;		
+		//long phNo=9876543210;
+		
+			
+		System.out.println("The brand name of the Car :"+brandName);
+		System.out.println("Car logo is : "+logo);	
+		System.out.println("price:"+price);
+		System.out.println("fuel:"+fuel);
+		System.out.println(modelVariant +": "+isNew);
+
+		//shortcut to execute the code -F11
+	}
+
+}
